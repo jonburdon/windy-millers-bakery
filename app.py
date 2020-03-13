d982925b34672e8bae1cc5645665c89282afd2e7
jonburdon <jon@branchoutwebsites.com>
2020-03-13 00:48:02 +0000

    connected to Mongo with test index.html. Bug - won't print categories yet

--- app.py
+++ app.py
@@ -3,4 +3,31 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
+from os import path 
+if path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
+
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app.config["MONGO_DBNAME"] = 'windy-millers_bakery'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
+
+from flask import Flask, render_template, redirect, request, url_for
+
+@app.route('/')
+@app.route('/index')
+def home():
+    test = list(mongo.db.categories.find())
+    for t in test:
+        print(t)
+    return render_template("index.html" )
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',
+            port=(os.environ.get('PORT')),
+            debug=True)
