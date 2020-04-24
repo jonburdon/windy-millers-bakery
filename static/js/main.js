f6edcd71a350a1fd3fa7d80d0f0bb025c163d734
jonburdon <jon@branchoutwebsites.com>
2020-04-24 09:00:44 +0100

    Update comments in main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,54 @@
-var $grid = $('.grid-main').isotope({
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows',
-    getSortData: {
-        name: function(element) {
-            return $(element).text();
-        },
-        cookingtime: '.cookingtime parseInt',
-    }
-});
-$('.filter li .filteroption').on("click", function() {
-    var value = $(this).attr('data-name');
-    $grid.isotope({
-        filter: value
+// ** USE ISOTOPE FRAMEWORK FOR FILTERING AND SORTING **
+$(document).ready(function() {
+
+    // ----------- * FILTERING * -----------
+
+    // Filter by category 
+    $('.filter li .filteroption').on("click", function() {
+        var value = $(this).attr('data-name');
+        $grid.isotope({
+            filter: value,
+            layoutMode: 'fitRows'
+        });
+        // Remove active class from all
+        $('.filter li .filteroption').removeClass('active');
+        // Add active class to 'this'
+        $(this).addClass('active');
     });
-    $('.filter li .filteroption').removeClass('active');
-    $(this).addClass('active');
+
+
+    // ----------- * SORTING * -----------
+    // Sort by cooking time
+
+    var $grid = $('.grid-main').isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows',
+        getSortData: {
+            name: function(element) {
+                return $(element).text();
+            },
+            cookingtime: '.cookingtime parseInt',
+        }
+    });
+
+    // Sort by name, original or random 
+    $('.sort li a').on("click", function() {
+        var value = $(this).attr('data-name');
+        $grid.isotope({
+            sortBy: value
+        });
+        // Remove active class from all
+        $('.sort li a').removeClass('active');
+        // Add active class to this
+        $(this).addClass('active');
+    })
+
+
+
+
 });
 
 
-$('.sort li a').on("click", function() {
-    var value = $(this).attr('data-name');
-    $grid.isotope({
-        sortBy: value
-    });
-    $('.sort li a').removeClass('active');
-    $(this).addClass('active');
-})
 
 
 /* Toggle Hamburger when close / open modal
@@ -36,51 +59,56 @@ $hamburger.on("click", function(e) {
 });
 */
 
+// ----------- * HEADER * -----------
 
 // Script below adapted from https://elementorcodes.com/elementor-header-show-scroll-up/
-
-  document.addEventListener('DOMContentLoaded', function() {
-  jQuery(function($){
-  var mywindow = $(window);
-  var mypos = mywindow.scrollTop();
-  mywindow.scroll(function() {
-  if (mypos > 120) {
-  if(mywindow.scrollTop() > mypos) {
-  $('#stickyheaders').addClass('headerup');
-  } else {
-  $('#stickyheaders').removeClass('headerup');
-  }
-  }
-  mypos = mywindow.scrollTop();
-  }); }); });
+// Remove sticky header on scroll down and reveals on scroll up (from any page location)
+document.addEventListener('DOMContentLoaded', function() {
+    jQuery(function($) {
+        var mywindow = $(window);
+        var mypos = mywindow.scrollTop();
+        mywindow.scroll(function() {
+            if (mypos > 120) {
+                if (mywindow.scrollTop() > mypos) {
+                    $('#stickyheaders').addClass('headerup');
+                } else {
+                    $('#stickyheaders').removeClass('headerup');
+                }
+            }
+            mypos = mywindow.scrollTop();
+        });
+    });
+});
 // End of adapted snippet from https://elementorcodes.com/elementor-header-show-scroll-up/
 
 
-// Show and hide featured images on when associated button is hovered //
-  $(document).ready(function() {
-    $('.featured-utensil-button-2').hover(function(){     
-          $('.featured-utensil-image-1').addClass('display-2');
-          $('.featured-utensil-image-1').removeClass('display-1');
-          $('.featured-utensil-image-2').removeClass('display-2');
-          $('.featured-utensil-image-2').addClass('display-1');
-      },function(){    
-         $('.featured-utensil-image-2').addClass('display-2');
-         $('.featured-utensil-image-2').removeClass('display-1');
-         $('.featured-utensil-image-1').addClass('display-1');
-         $('.featured-utensil-image-1').removeClass('display-2');     
-      });
+// ----------- * HIDE & SHOW * -----------
 
-      $('.featured-utensil-button-3').hover(function(){     
+// Show and hide featured images in recipe.html  when associated button is hovered //
+$(document).ready(function() {
+    $('.featured-utensil-button-2').hover(function() {
+        $('.featured-utensil-image-1').addClass('display-2');
+        $('.featured-utensil-image-1').removeClass('display-1');
+        $('.featured-utensil-image-2').removeClass('display-2');
+        $('.featured-utensil-image-2').addClass('display-1');
+    }, function() {
+        $('.featured-utensil-image-2').addClass('display-2');
+        $('.featured-utensil-image-2').removeClass('display-1');
+        $('.featured-utensil-image-1').addClass('display-1');
+        $('.featured-utensil-image-1').removeClass('display-2');
+    });
+
+    $('.featured-utensil-button-3').hover(function() {
         $('.featured-utensil-image-1').addClass('display-2');
         $('.featured-utensil-image-1').removeClass('display-1');
         $('.featured-utensil-image-3').removeClass('display-3');
         $('.featured-utensil-image-3').addClass('display-1');
-    },function(){    
-       $('.featured-utensil-image-3').addClass('display-3');
-       $('.featured-utensil-image-3').removeClass('display-1');
-       $('.featured-utensil-image-1').addClass('display-1');
-       $('.featured-utensil-image-1').removeClass('display-2');     
+    }, function() {
+        $('.featured-utensil-image-3').addClass('display-3');
+        $('.featured-utensil-image-3').removeClass('display-1');
+        $('.featured-utensil-image-1').addClass('display-1');
+        $('.featured-utensil-image-1').removeClass('display-2');
     });
 
 
-  });
+});
